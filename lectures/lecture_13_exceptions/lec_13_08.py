63029e156dff93a8ac8c21861032f2787f4280c0
Nikita <nikvs@mail.ru>
2023-07-13 01:25:49 +0700

    Лекция 13

---
+++ lectures/lecture_13_exceptions/lec_13_08.py
@@ -0,0 +1,12 @@
+# Исключения
+# Блок finally без except
+
+file = open('text.txt', 'r', encoding='utf-8')
+try:
+    data = file.read().split()
+    print(data[len(data)])
+finally:
+    print('Закрываю файл')
+    file.close()
+
+
