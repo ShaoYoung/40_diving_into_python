80c022b7acdfc3282bd971b98f7251c8c161fdb1
Nikita <nikvs@mail.ru>
2023-07-20 02:45:43 +0700

    ДЗ к семинару 14

---
+++ Homeworks/HW_14/hw_14.py
@@ -0,0 +1,54 @@
+# Задание №6
+# 📌 На семинаре 13 был создан проект по работе с пользователями (имя, id, уровень).
+# 📌 Напишите 3-7 тестов pytest для данного проекта.
+# 📌 Используйте фикстуры.
+
+import pytest
+import sys
+
+sys.path.append('..\\..\\seminars\\seminar_13')
+from sem_13_05 import Project
+from sem_13_03 import AccessException, LevelException
+from sem_13_04 import User
+
+
+
+@pytest.fixture
+def project_1():
+    filename = '..\\..\\seminars\\seminar_13\\users.json'
+    return Project.load(filename)
+
+
+# создание экземпляра класса Project
+def test_create(project_1):
+    assert isinstance(project_1, Project), 'Something is not OK'
+
+
+# логин с ошибкой доступа
+def test_login_ban(project_1):
+    with pytest.raises(AccessException):
+        project_1.login('777', 'qwerty')
+
+
+# разрешённый логин
+def test_login(project_1):
+    project_1.login('006', 'Люся')
+    assert User('006', 'Люся') in project_1.users and project_1.admin.name == 'Люся', 'User did not login'
+
+
+# добавление пользователя без администратора в проекте
+def test_add_user(project_1):
+    with pytest.raises(AttributeError):
+        project_1.add_user('777', 'qwerty', 0)
+
+
+# удаление пользователя из проекта
+def test_del_user(project_1):
+    user_del = User('010', 'Мустафа', '7')
+    project_1.del_user(user_del)
+    assert user_del not in project_1.users, 'User did not delete'
+
+
+if __name__ == '__main__':
+    # ['-v'] - для подробных выводов
+    pytest.main(['-v'])
